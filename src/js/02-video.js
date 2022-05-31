af67340566256f162b7d20f0ce5c6e16e4f077f8
Adrianna Staniek <adrstani@wp.pl>
2022-05-31 23:46:52 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,14 @@
+
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+const _ = require('lodash');
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', throttle(currentTime, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+function currentTime(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
 
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
