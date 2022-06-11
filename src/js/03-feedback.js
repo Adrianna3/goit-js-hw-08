c7e017dc609d7e178d94f965722b1ff9dfac7c25
Adrianna Staniek <adrstani@wp.pl>
2022-06-11 03:10:18 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+const { throttle } = require("lodash");
 
+const form = document.querySelector(".feedback-form");
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+// updateEmail();
+// updateMessage();
+
+form.addEventListener('input', throttle(setItemValue, 500))
+
+function setItemValue() {
+    const valueInput = {
+        email: email.value,
+        message: message.value,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(valueInput));
+}
+ 
+function updateInput() {
+  let inputValue = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (inputValue === null) {
+    return;
+  }
+  email.value = inputValue.email;
+  message.value = inputValue.message;
+};
+
+updateInput();
+
+form.addEventListener('submit', saveMessage);
+ 
+function saveMessage(evt) {
+    evt.preventDefault();
+    console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');  
+  this.reset();
+}
